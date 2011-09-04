e57555e143587d3c726e364c770d97d6070a6ade
laihui0207 <laihui0207gm@gmail.com>
2011-09-05 00:06:59 +0800

    first event

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunevents::Application.config.secret_token = '13508b6e7efd1e4ad69a96653e1dc3a1ccced6aee4e1e053972045b443b02b866fffd5abc16e2edfea23bc7c733ef89bfa7345fc72bce51eda4431b433899985'
